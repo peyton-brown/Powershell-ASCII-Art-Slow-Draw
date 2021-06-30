1fd858fccdd35ab7353e4c3a16cc859e5dd168b9
Peyton Brown <peyton.brown@intra.qsibanking.com>
2021-06-30 07:53:59 -0400

    Initial Commit

---
+++ slow-draw.ps1
@@ -0,0 +1,62 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+Clear-Host
+
+function Write-Slowly ($text,$color) {
+    if (!($color -match "Black|Blue|Cyan|DarkBlue|DarkCyan|DarkGray|DarkGreen|DarkMagenta|DarkRed|DarkYellow|Gray|Green|Magenta|Red|Yellow")) {$color = "white"}
+    $i = 0
+
+    while ($i -le ($text).length) {
+        Write-Host "$($text[$i])" -noNewline -foregroundcolor $color
+        Start-Sleep -m .5
+        ++$i
+    }
+
+    Write-Host
+}
+
+while($true) {
+    Start-Sleep -s 1
+    Write-Slowly "
+
+
+
+
+
+
+                                         ``````````````````
+                                        `/////////////////:
+                                      `////:-:///////////////
+                                      `////-.:///////////////
+                                      `//////////////////////
+                                      `//////////////////////
+                                      `//////////////////////
+                                      `//////////:
+                                      `///////////::::::-
+                .::                 .::////////:`````````
+                ./:              ---://////////-
+                ./:--        `---://///////////:----`
+                .////-.`   `.-/////////////////:--//`
+                .//////-...-///////////////////- `::`
+                .//////////////////////////////-
+                .::////////////////////////////-
+                  `//////////////////////////:..
+                    `////////////////////////-
+                     ``:///////////////////-``
+                       ```-://///////////```
+     .-----------------`   -//////-.-////    .------------------------------.
+                           .////-.` `.-//
+            .....          ./:--`     `//                         .--.
+            `````          ./:..      `//..`       .
+     .----          `--`   .::::`     `::::.       .      `----`
+
+
+
+
+
+
+
+
+" Cyan # Enter your ASCII text in the quotes, change the color of the text by changing Green to any color in the write-slowly function.
+    Start-Sleep -s 5
+    Clear-Host
+}
